e94cd34950190478467105e3ade629f311d7d930
morteza <morteza.kh.r@gmail.com>
2020-10-23 02:58:10 +0330

    first commit

---
+++ hanoi.c
@@ -0,0 +1,20 @@
+﻿/* in the name of Allah */
+
+#include <stdio.h>
+
+#define say(from, to)                                                                                        \
+	do {                                                                                                     \
+		printf("move from %c to %c\n", from, to);                                                            \
+		fflush(stdout);                                                                                      \
+	} while (0)
+
+void move(int num, int from, int to, int tmp) {
+	if (num == 1) {
+		say(from, to);
+	}
+	else {
+		move(num - 1, from, tmp, to);
+		move(1, from, to, tmp);
+		move(num - 1, tmp, to, from);
+	}
+}
---
+++ line_calculator.c
@@ -0,0 +1,99 @@
+﻿/* in the name of Allah */
+
+#include <stdio.h>
+#include <assert.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+void const *memdup(void const *, size_t, size_t);
+long long get_digit(char const **);
+long long next_opd(char const **);
+long long calculate(char const **);
+
+#define is_opr(c) (c == '+' || c == '-' || c == '*' || c == '/')
+
+#define __skip(ptr, cond)                                                                                      \
+	do {                                                                                                     \
+		for (; cond(*(ptr)); (ptr)++)                                                                        \
+			;                                                                                                \
+	} while (0)
+
+#define skip_white(ptr) __skip(ptr, isspace)
+
+#define skip_digit(ptr) __skip(ptr, isdigit)
+
+#define ptr_advance(ptr) ++(ptr)
+
+#define next(ptr) (*(ptr_advance(*ptr) - 1))
+
+#define str_dup(data, length) (char const *)memdup((char const *)data, length, sizeof(char))
+
+void const *memdup(void const *data, size_t size, size_t size_of) {
+	void *dptr = calloc(size, sizeof(size_of));
+	assert(dptr != NULL);
+
+	return memcpy(dptr, data, size * size_of);
+}
+
+long long get_digit(char const **sptr) {
+	char const *dummy = *sptr;
+	skip_digit(*sptr);
+	char const *dptr = str_dup(dummy, *sptr - dummy);
+	long long digit = atoll(dptr);
+
+	return free((char *)dptr), digit;
+}
+
+long long calculate(char const **sptr) {
+	long long opd = 0;
+
+	if (**sptr == ')') {
+		return opd;
+	}
+	if (**sptr != 0) {
+		opd = next_opd(sptr);
+		skip_white(*sptr);
+
+		if (is_opr(**sptr)) {
+			int opr = next(sptr);
+			switch (opr) {
+				case '*': return opd * next_opd(sptr);
+				case '/': {
+					long long op2 = next_opd(sptr);
+					assert(op2 != 0);
+					return opd / op2;
+				}
+				case '+': return opd + calculate(sptr);
+				case '-': return opd - calculate(sptr);
+			}
+		}
+		return opd + calculate(sptr);
+	}
+	return opd;
+}
+
+long long next_opd(char const **sptr) {
+	long long opd = 0;
+
+	skip_white(*sptr);
+	if (isdigit(**sptr)) {
+		opd = get_digit(sptr);
+	}
+	else if (**sptr == '(') {
+		(void)(next(sptr));
+		opd = calculate(sptr);
+		skip_white(*sptr);
+		assert(next(sptr) == ')');
+	}
+	return opd;
+}
+
+int main() {
+	char line[1024];
+	
+	while (fgets(line, 1024, stdin)) {
+		char const *lineptr = str_dup(line, strlen(line));
+		printf("result: %lld\n", calculate(&lineptr));
+	}
+}
